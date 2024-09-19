a07783fec9e87cf6d64c40ff62c44e26fe44b446
teebliqs4@gmail.com <teebliqs4@gmail.com>
2024-09-19 07:57:22 +0100

    phone-fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { Provider } from "react-redux";
 import { store } from "../app/store";
 import Footer from "@/components/Footer";
 import ToggleButton from "@/components/ToggleButton";
+import Sidebar from "@/components/Sidebar";
+import { Head } from "next/document";
 
 const oswald = Oswald({
   subsets: ['latin'],
@@ -14,12 +16,15 @@ const oswald = Oswald({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={oswald.className}>
+       <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </Head>
       <body className="flex flex-col h-screen">
         <Provider store={store}>
           <div className="flex flex-1">
             {/* Sidebar */}
             <div className="w-72 lg:w-1/4 bg-gray-200 dark:bg-gray-800">
-              {/* <Sidebar /> */}
+              <Sidebar />
             </div>
 
             {/* Main content area */}
